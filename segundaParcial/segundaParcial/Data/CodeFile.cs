b3a3c49f2d0c41a319ed448b596dc601e9335fb5
JosueBlas22 <149421380+JosueBlas22@users.noreply.github.com>
2024-04-15 11:25:23 -0600

    Add files via upload

---
+++ segundaParcial/segundaParcial/Data/CodeFile.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace segundaParcial.Data
+{
+    public class Persona
+    {
+        [Required(ErrorMessage = "El # de inventario es Reuerido")]
+        [MaxLength(100, ErrorMessage = "Debe ser un valor positivo y unico Valido")]
+        public string? Inventario { get; set; }
+
+        [Required(ErrorMessage = "Requiere nombre de Producto")]
+        [MaxLength(10, ErrorMessage = "Maximo 10 letras")]
+        public string? Nombre { get; set; }
+
+
+        public enum Departamento
+        {
+            Frutas,
+            Verduras,
+            Lacteos
+        }
+        public Departamento SelectedDepartamento { get; set; }
+        
+
+    }
+}
